387a49becb1f3ef0f9c0b187007db3e37d35421e
unknown <thomas1228@student.babcock.edu.ng>
2021-05-31 16:58:18 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAU4uv_7UNnKhg_c71Oidde9ZFI2J7QEFM",
+    authDomain: "fir-angular-auth-46a61.firebaseapp.com",
+    projectId: "fir-angular-auth-46a61",
+    storageBucket: "fir-angular-auth-46a61.appspot.com",
+    messagingSenderId: "995107071202",
+    appId: "1:995107071202:web:34ea6df29f68536142eabb"
+  }
 };
